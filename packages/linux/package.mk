62b6f1911cac0c63bc6cfc8629dced34c391c265
Arne Bockholdt <a.bockholdt@precitec-optronik.de>
2020-01-12 17:47:36 +0100

    KERNEL: Update to 5.4.4

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -37,8 +37,8 @@ case "$LINUX" in
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
   *)
-    PKG_VERSION="5.4.3"
-    PKG_SHA256="6731682f32e1b1ee53b0e7f66b8dc263d25a0e809e78e2139cb0ed77c378ee51"
+    PKG_VERSION="5.4.4"
+    PKG_SHA256="3fa2aad785f8031246b25362d7542dac46aaaa91546fb41b6419fb759e43e6c2"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
