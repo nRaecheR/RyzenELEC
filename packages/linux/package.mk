acfc36db82ecd3c1ca007d75d0821c211ecc6567
Arne Bockholdt <a.bockholdt@precitec-optronik.de>
2020-06-15 09:38:54 +0200

    KERNEL: Update to 5.6.15

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -29,8 +29,8 @@ case "$LINUX" in
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
   *)
-    PKG_VERSION="5.6.13"
-    PKG_SHA256="f125d79c8f6974213638787adcad6b575bbd35a05851802fd83f622ec18ff987"
+    PKG_VERSION="5.6.15"
+    PKG_SHA256="65ab799393d490463c610270634874dfcb66440a312837d04b51bbb69323034e"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
