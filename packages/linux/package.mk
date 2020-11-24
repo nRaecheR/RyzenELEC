f9762bc6f3425f06b83b850208a8f427cf5aa592
Arne Bockholdt <a.bockholdt@precitec-optronik.de>
2020-11-24 17:25:01 +0100

    KERNEL: Update to 5.9.11

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -22,8 +22,8 @@ case "$LINUX" in
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
   *)
-    PKG_VERSION="5.9.9"
-    PKG_SHA256="a302d386af1278e7a8c0c2cd9a3b2119a18620eccc1f069b0f23e405bcf61fad"
+    PKG_VERSION="5.9.11"
+    PKG_SHA256="5eb20a65a410669928f94b3975872e493fa6d0fe441c6a78b7564affa2a5d260"
     PKG_URL="https://www.kernel.org/pub/linux/kernel/v5.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
