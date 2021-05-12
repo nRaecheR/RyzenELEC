cafa837f3aae9ee30658ae38d98198db4a1f2372
Arne Bockholdt <a.bockholdt@precitec-optronik.de>
2021-05-12 09:05:13 +0200

    MESA: Update to 21.1.0, dropping unneeded patches

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="21.0.3"
-PKG_SHA256="565c6f4bd2d5747b919454fc1d439963024fc78ca56fd05158c3b2cde2f6912b"
+PKG_VERSION="21.1.0"
+PKG_SHA256="0128f10e22970d3aed3d1034003731f94623015cd9797c07151417649c1b1ff8"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
